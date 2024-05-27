4716b2111770e706aa40ba8466d3dda08015d708
Crowdbotics <team@crowdbotics.com>
2024-05-27 15:24:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "trader-48120",
+  url: "https://trader-48120.botics.co",
+  api: "https://trader-48120.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
